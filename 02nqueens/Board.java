d38ee5136e91fdb8bfe7d59fc368149850c2a57d
Jackson Morgan <jmorgan@stuy.edu>
2016-02-09 21:13:24 -0500

    added remove queen

--- 02nqueens/Board.java
+++ 02nqueens/Board.java
@@ -43,6 +43,31 @@ public class Board{
 	}
 	
     }
+
+    public void removeQueen(int n, int k){
+	board[n][k] = 0;
+	for(int i = 1; i < board[n].length - k ; i ++){
+	    board[n][k + i] ++;
+	}
+	for(int i = 1; k - i >= 0 ; i ++){
+	    board[n][k - i] ++;
+	} 
+
+	for(int i = 0; i + n < board.length - 1 && k + i < board.length -1; i ++){
+	    board[n + i + 1][k + i + 1] ++ ;
+	}
+	for(int i = 1;  n - i >= 0 && k + i < board.length; i ++){
+	    board[n - i ][k + i] ++ ;
+	}
+
+	for(int i = 0; n - i > 0 && k - i > 0 ; i ++){
+	  board[n - i - 1][k - i -1] ++ ;
+		}
+	for(int i = 1;  i + n < board.length && k - i >= 0; i ++){
+	  board[n + i ][k - i] ++ ;
+	}
+	
+    }
 	    
 	    
 	
@@ -53,7 +78,8 @@ public class Board{
     public static void main(String[]args){
 	Board f = new Board(6);
 	System.out.println(f.name());
-	f.addQueen(5, 0);
+	f.addQueen(2, 3);
+	f.removeQueen(2, 3);
 	f.printBoard();
     }
        
