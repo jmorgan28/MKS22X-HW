c6ae7b00e0ce292b9a262f359f4b242f1c05ea7e
Jackson Morgan <jmorgan@stuy.edu>
2016-02-11 13:07:37 -0500

    try

---
+++ KnightBoard.java
@@ -0,0 +1,25 @@
+public class KnightBoard{
+    private int[][] board;
+    public KnightBoard(int n){
+	board = board[n + 4][n +4];
+	for(int i = 0; i < board.length; i++){
+	    for(int k = 0; k < 2; k ++){
+		board[i][k] = -1;
+	    }
+	}
+
+    }
+
+    public boolean solveH(){
+	return false;
+    }
+
+    public boolean solve(){
+	return false;
+    }
+
+    public void printSolution(){
+
+    }
+	 
+}
