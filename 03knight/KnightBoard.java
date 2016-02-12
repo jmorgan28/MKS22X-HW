f15452b78e169fe3662ab198b43a710d697c74f7
Jackson Morgan <jmorgan@stuy.edu>
2016-02-12 12:38:19 -0500

    reconfigure file placement

--- 03knight/KnightBoard.java
+++ 03knight/KnightBoard.java
@@ -1,7 +1,28 @@
 public class KnightBoard{
-    private int[][] board; 
-    
+    private int[][] board;
     public KnightBoard(int n){
-	board = int[n+4][n+4];
+	board = board[n + 4][n +4];
+	for(int i = 0; i < board.length; i++){
+	    for(int k = 0; k < 2; k ++){
+		board[i][k] = -1;
+	    }
+	}
+
     }
+
+    public boolean solveH(){
+	return false;
+    }
+
+    public boolean solve(){
+	return false;
+    }
+
+    public void printSolution(){
+
+    }
+	 
+}public class KnightBoard{
+    private int[] board;
+    public KnightBoard
 }
