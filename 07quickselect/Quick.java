22dbe2e2b8867e7ec571d3800b0989c31f06aa1d
Jackson Morgan <jmorgan@stuy.edu>
2016-03-07 21:55:40 -0500

    start of 07

---
+++ 07quickselect/Quick.java
@@ -0,0 +1,14 @@
+public class Quick{
+    int [] data = {2,4324,-1234,295,3,55,62,305838554,2030,-19394,0,293940,105,15,39230,9420304,-103399};
+
+    public void print(){
+	for(int r = 0; r < data.length; r ++){
+	    System.out.println(data[r] + ' ');
+	}
+    }
+	
+
+
+
+
+}
