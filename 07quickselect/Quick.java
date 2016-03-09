746f7d45bab6ae3b7520116cc956d4c59d94105a
Jackson Morgan <jmorgan@stuy.edu>
2016-03-08 20:28:16 -0500

    partition done for real this time

--- 07quickselect/Quick.java
+++ 07quickselect/Quick.java
@@ -5,9 +5,10 @@ public class Quick{
 
     private static int partition(int[]data, int left, int right){
 	int split = (int)(Math.random() * (right - left))+ left;
-	System.out.println(split);
+	//System.out.println(split);
 	//int [] store = new int[right - left];
 	int store = data[split];
+	//System.out.println(store);
 	data[split] = data[right];
 	data[right] = store;
 	int transfer = 0;
@@ -38,55 +39,30 @@ public class Quick{
 	  
 	 
 	}
-	//transfer = data[i -1];
-	//	data[i - 1] = data[store];
-	//	data[remain] = transfer;
-		
-	return split;
-    }
-	// slow version
-	/*int more = 0;
-	int less = 0;
-	for(int i = 0; i + left < right; i ++){
-	    if(data[i + left] < store){
-		less ++;
-	    }
-	    if(data[i + left] > store){
-		more ++;
-	    }
+
+        //i = 0;
+	//while(data[i] < store){
+	    //i ++;
+	    //}
+	//transfer = data[i + 1];
+	//data[i + 1] = store;
+        //data[remain] = transfer;
+	if (data[right] >= store){
+	    transfer = data[right];
+	    data[right] = store;
+	    data[remain] = transfer;
+	    return right;
 	}
-	System.out.println(more);
-	System.out.println(less);
+	else{
+	    transfer = data[right + 1];
+	    data[right + 1] = store;
+	    data[remain] = transfer;
+	    return right + 1;
+	}
 	
-	int [] m = new int[more];
-	int [] l = new int[less];
-	int a = 0;
-	int b = 0;
-	for(int i = 0; i + left < right; i ++){
-	    if(data[i + left] < store){
-		l[a] = data[i + left];
-		a ++;
-	    }
-	    if(data[i + left] > store){
-		m[a] = data[i + left];
-		b ++;
-	    }
-	}
-	a = 0;
-	b = 0;
-	for(int i = 0; i + left < right; i ++){
-	    if( a < l.length){
-		data[i + left] = l[a];
-		a ++;
-	    }
-	    if( b < m.length && a >= l.length){
-		data[i + left] = m[b];
-		b ++;
-	    }
-	}
-       return split;
-       }*/
-    
+		
+    }
+	
 	    
 	    
     	public static String name(){
@@ -103,7 +79,7 @@ public class Quick{
 
     public static void main(String[]args){
 	int [] data = {2,4324,-1234,295,3,55,62,305838554,2030,-19394,0,293940,105,15,39230,9420304,-103399};
-	partition(data,0,data.length -1);
+	System.out.println(partition(data,0,data.length -1));
 	print(data);
     }
 	
