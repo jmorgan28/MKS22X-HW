c62924b7f643ee4ebcfc1060e74c49832ba99b56
Jackson Morgan <jmorgan@stuy.edu>
2016-03-15 13:06:41 -0400

    did add and tostring. didn't check if it worked

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -2,6 +2,33 @@ public class MyLinkedList{
     private LNode start;
     private int size;
 
+    //constructor
+
+    public boolean add(int value){
+	LNode store = start;
+	while(store.getNext() != null){
+	    store = start.getNext();
+	}
+	store.getNext().setValue(value);
+	return true;
+    }
+
+    public String toString(){
+	String s = "[";
+	LNode store = start;
+         while(store.getNext() != null){
+	     s += start.getNext().getValue();
+	     s += ", ";
+	}
+	s += store.getNext().getValue();
+	s += "]";
+	return s;
+    }
+
+	
+	
+	
+
     public class LNode{
 	private int value;
 	private LNode next; 
@@ -19,7 +46,7 @@ public class MyLinkedList{
 	}
 	    
 	public LNode getNext(){
-	    return LNode;
+	    return next;
 	}
 	
 	public void setNext(LNode node){
