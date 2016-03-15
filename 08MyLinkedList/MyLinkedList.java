2204ef79d2c19312c24774169479b20d889a1607
Jackson Morgan <jmorgan@stuy.edu>
2016-03-15 12:52:53 -0400

    started 08

---
+++ 08MyLinkedList/MyLinkedList.java
@@ -0,0 +1,33 @@
+public class MyLinkedList{
+    private LNode start;
+    private int size;
+
+    public class LNode{
+	private int value;
+	private LNode next; 
+	
+	public LNode(int numb){
+	    value = numb;
+	}
+
+	public int getValue(){
+	    return value;
+	}
+
+	public void setValue(int val){
+	    value = val;
+	}
+	    
+	public LNode getNext(){
+	    return LNode;
+	}
+	
+	public void setNext(LNode node){
+	    next = node;
+	}
+    }
+	    
+	    
+	    
+
+}
