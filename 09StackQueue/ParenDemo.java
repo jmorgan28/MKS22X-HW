9780636a70375a7efbbd5b19e6edef7a1a608151
Jackson Morgan <jmorgan@stuy.edu>
2016-03-29 18:17:59 -0400

    may be done

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -2,17 +2,65 @@ public class ParenDemo{
     
     public static boolean isMatching(String s){
 	MyStack<Character> m = new MyStack<Character>();
-	for(int i = s.length() -1; i >= 0; i--){
+	MyStack<Character> f = new MyStack<Character>();
+	for(int i = 0; i <  s.length(); i++){
+	    if(s.charAt(i) == ('(')){
 	    m.push(s.charAt(i));
-	}
-	if(s.charAt(i).equals('(')){
-	    if(m.peek.equals(')')){
-		
-  
-	
-	
+		}
+	    if(s.charAt(i) == ('[')){
+		m.push(s.charAt(i));
+	    }
+	    if(s.charAt(i) == ('{')){
+		m.push(s.charAt(i));
+	    }
 
-	return true;
+	    if(s.charAt(i) ==('<')){
+		m.push(s.charAt(i));
+	    }
+	}
+	for(int i = s.length() -1; i > 0; i--){
+	    if(s.charAt(i) == (')')){
+	    f.push(s.charAt(i));
+		}
+	    if(s.charAt(i) == (']')){
+		f.push(s.charAt(i));
+	    }
+	    if(s.charAt(i) == ('}')){
+		f.push(s.charAt(i));
+	    }
+
+	    if(s.charAt(i) ==('>')){
+		f.push(s.charAt(i));
+	    }
+	}
+	if(m.size() != f.size()){
+	    return false;
+	}
+	int k = 0;
+	while(k < m.size()){
+	    if(m.peek() == '(' && f.peek() != ')'){
+		return false;
+	    }
+	    else{
+	    if(m.peek() == '{' && f.peek() != '}'){
+		return false;
+	    }else {
+   
+	    
+	    if(m.peek() == '[' && f.peek() != ']'){
+		return false;
+	    }else{
+	    
+	    if(m.peek() == '<' && f.peek() != '>'){
+		return false;
+	    }}}}
+	    m.pop();
+	    f.pop();
+	    k ++;
+	}
+	    
+	 
+       	return true;
 	
     }
     
