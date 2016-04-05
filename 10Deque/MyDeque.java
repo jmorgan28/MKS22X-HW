d54c59a74f6fd8e64285f6b35e2c3778287a2456
Jackson Morgan <jmorgan@stuy.edu>
2016-04-04 22:13:48 -0400

    some progress

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,32 +1,73 @@
 public class MyDeque<T>{
-    private Object[] data;
+    T[] data;
     private int start;
     private int end;
     private int size;
 
     public MyDeque(){ 
-	data = new Object[10];
+       data = (T[]) new Object[10];
     }
 
     private void resize(){
-	Object[] temp = new Object[data.length * 2];
+	T[] temp = (T[])new Object[data.length * 2];
 	for(int i = 0; i < data.length; i ++){
-	    temp[i] = data[i];
+	    if(start + i < data.length){
+	    temp[i +1] = data[start + i];
+	    }
+	    else{
+		temp[i + 1] = data[start + i -start];
+	    }
+		
 	}
 	data = temp;
+	start = 1;
+	end = start + size -1;
     }
 
     public void addFirst(T value){
 	if(size == 0){
-	    start = 0;
-	    end = 0;}
-	else{start ++;}
+	    start = 4;
+	    end = 4;}
+	else{
+	    if(start - 1 == end){
+		resize();
+	}
+	    if(start == 0 && end < data.length -1){
+	    start = data.length -1;
+	    }else{
+	    start --;}}
+    
 	data[start] = value;
 	size ++;
     }
 
-    public void addLast(T value){
-	
+    public void print(){
+	System.out.print("[");
+	for(int i = 0; i < data.length; i ++){
+	    System.out.print(data[i] + ", ");
+	}
+	System.out.print("]");			 
+    }
+
     
+
+    //public void addLast(T value){
+	
+    public static void main(String[]args){
+	MyDeque<Integer> d = new MyDeque<Integer>();
+	d.addFirst(3);
+	d.addFirst(4);
+	d.addFirst(5);
+	d.addFirst(6);
+	d.addFirst(7);
+	d.addFirst(9);
+	d.addFirst(10);
+	d.addFirst(11);
+	d.addFirst(12);
+	d.addFirst(13);
+	d.addFirst(14);
+	d.print();
+    }
+	
     
 }
