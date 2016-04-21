0a51e84008a00f8ecadd1c56319e47638a682737
Jackson Morgan <jmorgan@stuy.edu>
2016-04-21 13:00:54 -0400

    added set and gets

--- 12BStree/BSTree.java
+++ 12BStree/BSTree.java
@@ -1,64 +1,73 @@
-public class BSTree<T extends comparable<T>>{
+public class BSTree<T extends Comparable<T>>{
     private class Node{
- T data;
- Node left;
- Node right;
- // set/get: data/left/right
+	T data;
+	Node left;
+	Node right;
+	// set/get: data/left/right
+
+	public void setData(T value){
+	    data = value;
+	}
+	
+	public T getData(){
+	     return data;
+	}
+
+	 public void setLeft(Node n){
+	    left = n;
+	}
+	
+	public Node getLeft(){
+	     return left;
+	}
+
+	public void setRight(Node n){
+	    right = n;
+	}
+	
+	public Node getRight(){
+	     return right;
+	}
 
 
- //real methods here
- public int height(){ 
-    return 0;
- }
- public void add(T value){
- }
- public String toString(){
-    return "";
- }
- public boolean contains(T value){
-    return false;
- }
+	//real methods here
+	public int height(){ 
+	    return 0;
+	}
+	public void add(T value){
+	    data = value;
+	}
+	public String toString(){
+	    return "" + data;
+	}
+	public boolean contains(T value){
+	    return value == data;
+	}
     
     }
 
     private Node root;
 
     //OUTER methods here are wrapper methods for the root
-    public getHeight(){
- //call the root's methods
- //check for empty first!
- return root.height();
+    public int getHeight(){
+	//call the root's methods
+	//check for empty first!
+	return root.height();
     }
 
     public void add(T value){
- //check for empty before you do things with root.
+	//check for empty before you do things with root.
     }
     public String toString(){
- //check for empty before you do things with root.
- return "";
+	//check for empty before you do things with root.
+	return "";
     }
     public boolean contains(T value){
- //check for empty before you do things with root.
- return false;
+	//check for empty before you do things with root.
+	return false;
     }
-}public class BSTre<T extends Comparable<T>>{
-    
-    private class Node{
-	T data;
-	Node right;
-	Node left;
+}
 
-	public Node(T value){
-	    data = value;
-	}
-	
-	public void add(T value){}
-	public String toString{}
-	public boolean contains(T value){return false;}
-	public int getHeight(){return 0;}
-	
-
-    }
 
 
     
